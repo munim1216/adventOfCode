7198cc920f4cba9d1a809b7ba8267c996bc84eb1
munim1216 <145690615+munim1216@users.noreply.github.com>
2024-12-05 14:50:00 -0500

    Add files via upload

--- day2Part2.py
+++ day2Part2.py
@@ -13,31 +13,24 @@ def string_to_int_list(str): ## TODO FIX THIS STUPID MEHOD FROM DAY ONE!!!!!!!!!
     return int_list
 
 
-def check(report, idx, previous_difference, mistakes):
+def check(report, idx, previous_difference):
     if idx >= len(report) - 1:
         return True
-
-    if mistakes > 1:
-        return False
-
     difference = report[idx] - report[idx + 1]
     same_sign = (difference < 0 and previous_difference < 0) or (difference > 0 and previous_difference > 0)
     if abs(difference) > 3 or difference == 0 or not same_sign:
-        mistakes += 1
-        report.pop(idx)
-        return check(report, idx, previous_difference, mistakes)
+        return False
     if idx < len(report) - 1:
-        return check(report, idx + 1, difference, mistakes)
+        return check(report, idx + 1, difference)
     return True
 
 
 def first_check(report, idx):
     difference = report[idx] - report[idx + 1]
-    mistakes = 0
     if abs(difference) > 3 or difference == 0:
-        mistakes += 1
+        return False
     if idx < len(report) - 1:
-        return check(report, idx + 1, difference, mistakes)
+        return check(report, idx + 1, difference)
     return True
 
 
@@ -49,7 +42,14 @@ line = 1
 for data in file_data:
     print("Line: ", line, "Data: ", data)
     report = string_to_int_list(data.split(" "))
-    if first_check(report, 0):
+    safe = False
+    for i in range(len(report)):
+        temp_list = report.copy()
+        temp_list.pop(i)
+        if first_check(temp_list, 0):
+            safe = True
+
+    if safe:
         safe_reports += 1
     line += 1
 
