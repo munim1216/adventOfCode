0cf734334b006dd1db5a9fbe5a2f3213916667fd
munim1216 <145690615+munim1216@users.noreply.github.com>
2024-12-18 14:51:30 -0500

    Add files via upload

--- day_5_part_1.py
+++ day_5_part_1.py
@@ -1,4 +1,7 @@
+import array
+import math
 import re
+import string
 
 
 def get_file_data(file_name):
@@ -14,7 +17,6 @@ def get_file_data(file_name):
 
     line = file[idx]
     while not line.isspace():
-        print(file[idx])
         rules.append(file[idx][0:-1])
         idx += 1
         line = file[idx]
@@ -36,19 +38,28 @@ def rule_check(rule, page):
     after_num = re.findall("\\|.*", rule)[0][1:]
     before_idx = page.find(before_num)
     after_idx = page.find(after_num)
+    print(before_idx, after_idx)
 
-    return before_idx < after_idx
+    return before_idx < after_idx or (before_idx == -1 and after_idx == -1)
 
+
+def find_middle_num(number_string):
+    string_array = number_string.split(",")
+    return int(string_array[len(string_array) // 2])
 
 valid = []
+
 for page in pages:
     safe = True
-    rule_number = 0
-    while safe and rule_number < len(rules):
-        safe = rule_check(rules[rule_number], page)
-        rule_number += 1
+    for rule in rules:
+        safe = rule_check(rule, page)
 
     if safe:
         valid.append(page)
 
-print(valid)
+total = 0
+
+for valid_page in valid:
+    total += find_middle_num(valid_page)
+
+print(total)
